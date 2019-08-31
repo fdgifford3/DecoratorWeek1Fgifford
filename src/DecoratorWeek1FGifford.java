1551a48f046eea669061430747326826117c9120
fdgifford3 <fdgifford3@gmail.com>
2019-08-30 20:40:49 -0500

    Added several concrete classes and decorators

--- src/DecoratorWeek1FGifford.java
+++ src/DecoratorWeek1FGifford.java
@@ -1,13 +1,37 @@
-
+/*
+ * Frederick Gifford
+ * CPSC-60000 Week 1 Decorator Pattern with Baking Theme
+ * 8.30.2019
+ * Prof Nowak
+ */
 public class DecoratorWeek1FGifford {
 
 	public static void main(String[] args) {
+		System.out.println("Frederick Gifford\nCPSC-60000\nWeek 1 Decorator Pattern\n");
 		// Test some Cake Types, sizes, and decorators to see how it works
+		
+		// Cakes are instantiated with a size. I decided NOT to allow
+		// The Constructor to be overloaded with a no-argument method
+		// Because we shouldn't allow a 'default' size to accidently be used
 		Cake cake = new VanillaCake(Cake.Size.FOUR);
 		System.out.println(cake.getDescription() + " $" + cake.cost());
 		
+		// Here is where the decorator pattern is used, wrap the cake in the decorator
 		cake = new ButtercreamFrosting(cake);
 		System.out.println(cake.getDescription() + " $" + cake.cost());
+		
+		// Can also wrap in one step
+		Cake cake2 = new ButtercreamFrosting(new ChocolateCake(Cake.Size.SIX));
+		System.out.println(cake2.getDescription() + " $" + cake2.cost());
+
+		Cake cake3 = new CreamCheeseFrosting(new RedVelvetCake(Cake.Size.EIGHT));
+		System.out.println(cake3.getDescription() + " $" + cake3.cost());
+		
+		// Wrapping lots of add-ons
+		Cake cake4 = new Sprinkles(new SugarFlowers(new ButtercreamFlowers(new ButtercreamFlowers(
+				new ButtercreamFrosting(new ConfettiCake(Cake.Size.TEN))))));
+		System.out.println(cake4.getDescription() + " $" + cake4.cost());
+
 	}
 
 }
---
+++ src/RedVelvetCake.java
@@ -0,0 +1,26 @@
+
+
+public class RedVelvetCake extends Cake {
+	
+	public RedVelvetCake(Size size) {
+		this.size = size;
+		this.description = "Red Velvet Cake";
+	}
+	
+
+	public double cost() {
+		double cost = 0.00;
+		
+		if (this.size == Size.FOUR) {
+			cost = 21.00;
+		} else if (this.size == Size.SIX) {
+			cost = 32.00;
+		} else if (this.size == Size.EIGHT) {
+			cost = 44.00;
+		} else if (this.size == Size.TEN) {
+			cost = 65.00;
+		}
+		return cost;
+	}
+
+}
